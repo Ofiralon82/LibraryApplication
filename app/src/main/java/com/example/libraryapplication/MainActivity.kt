1dd23ac552ffb6687de37f46991e6107710a2502
ofiralon <ofir@bizzabo.com>
2021-06-09 13:53:52 +0300

    remote settings

--- app/src/main/java/com/example/libraryapplication/MainActivity.kt
+++ app/src/main/java/com/example/libraryapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.libraryapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.cloudinary.*;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
